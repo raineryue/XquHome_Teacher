54da5181595d6da85909862865507b47a84f4657
Rainer <it_jinlei.yue@163.com>
2017-08-01 17:08:31 +0800

    Initial Commit

---
+++ XquHome_Teacher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XquHome_Teacher
+//
+//  Created by Rainer on 2017/8/1.
+//  Copyright © 2017年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
